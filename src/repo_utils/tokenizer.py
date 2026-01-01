e351ada034228b487a4871e9a1bcbc8eac962445
Thomas Sounack <thomascjl.bernard@gmail.com>
2026-01-01 12:16:50 +0100

    linted tokenizer

--- src/repo_utils/tokenizer.py
+++ src/repo_utils/tokenizer.py
@@ -6,21 +6,51 @@ from docx import Document
 from PyPDF2 import PdfReader
 import tiktoken
 
+
 def extract_text_from_file(filepath):
     """
     Returns the contents of the processed files in the repository
     """
     ext = Path(filepath).suffix.lower()
     code_extensions = {
-        ".py", ".r", ".java", ".js", ".ts", ".tsx", ".jsx",
-        ".php", ".html", ".css", ".cpp", ".c", ".h", ".hpp",
-        ".cs", ".go", ".rs", ".swift", ".kt", ".scala", ".rb",
-        ".pl", ".lua", ".sh", ".md", ".toml", ".sample", ".groovy", ".php",
-        ".ipynb", ".yaml", ".dvc", ".ttl", ".yml"
+        ".py",
+        ".r",
+        ".java",
+        ".js",
+        ".ts",
+        ".tsx",
+        ".jsx",
+        ".php",
+        ".html",
+        ".css",
+        ".cpp",
+        ".c",
+        ".h",
+        ".hpp",
+        ".cs",
+        ".go",
+        ".rs",
+        ".swift",
+        ".kt",
+        ".scala",
+        ".rb",
+        ".pl",
+        ".lua",
+        ".sh",
+        ".md",
+        ".toml",
+        ".sample",
+        ".groovy",
+        ".php",
+        ".ipynb",
+        ".yaml",
+        ".dvc",
+        ".ttl",
+        ".yml",
     }
     if ext in code_extensions:
         return Path(filepath).read_text(encoding="utf-8", errors="ignore")
-    
+
     if ext == "":
         try:
             content = Path(filepath).read_text(encoding="utf-8")
@@ -51,7 +81,8 @@ def extract_text_from_file(filepath):
 
     else:
         raise ValueError(f"Unsupported file type: {ext}")
-    
+
+
 def tokenize_text(text, encoding_name="cl100k_base"):
     enc = tiktoken.get_encoding(encoding_name)
     tokens = enc.encode(text)
