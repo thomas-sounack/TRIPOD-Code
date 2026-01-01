6e0ab118e938d16322ff25ae2e389f219ef30105
Thomas Sounack <thomascjl.bernard@gmail.com>
2026-01-01 12:15:55 +0100

    init exposes classes and strings needed

--- src/llm_utils/__init__.py
+++ src/llm_utils/__init__.py
@@ -1,2 +1,4 @@
 from .llm_wrapper import LLM_wrapper
 from .structs import PaperAssessment, RepoAssessment
+from .repo_assessment_prompt import REPO_ASSESSMENT_PROMPT
+from .paper_assessment_prompt import PAPER_ASSESSMENT_PROMPT
